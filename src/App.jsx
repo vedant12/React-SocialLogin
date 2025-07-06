db6a6f8103a25c3f4d1068aef054d49247e95973
Vedant Malaikar <vmalaikar@gmail.com>
2025-07-06 11:35:54 +0530

    added fb login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { GoogleLogin } from '@react-oauth/google'
+import FacebookLogin from '@greatsumini/react-facebook-login';
 import * as jwt_decode from 'jwt-decode';
 
 function App() {
@@ -9,6 +10,12 @@ function App() {
     console.log(decoded);
   }
 
+  const responseFacebook = (response) => {
+    console.log(response);
+    // response contains: accessToken, userID, name, email, picture etc.
+  };
+
+
   const handleError = () => {
     console.log('Login failed');
   }
@@ -17,6 +24,10 @@ function App() {
     <>
       <h2>Login with Google</h2>
       <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+      <FacebookLogin appId='your-fb-app-id' autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
     </>
   )
 }
